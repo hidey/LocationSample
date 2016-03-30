0cadbbe0a43d86ba4daf193737b32213a97553cc
Hideyuki Kikuma <hideyuki.kikuma@gmail.com>
2016-03-31 01:12:09 +0900

    sample

---
+++ app/src/main/java/jp/hidey/locationsample/MainActivity.java
@@ -0,0 +1,35 @@
+package jp.hidey.locationsample;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @NeedsPermission({Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION})
+    public void fetchLocation() {
+        LocationManager mgr = (LocationManager) getSystemService(Activity.LOCATION_SERVICE);
+        mgr.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode,grantResults);
+
+    }
+}
